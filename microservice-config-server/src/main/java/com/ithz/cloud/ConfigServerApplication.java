c707c9ac2c2fa13a9bfe6caa9f2560c51571eb1b
baker <783009838@qq.com>
2017-12-04 17:14:24 +0800

    configServer add properties

---
+++ microservice-config-server/src/main/java/com/ithz/cloud/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.ithz.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author baker
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
