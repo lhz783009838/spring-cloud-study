a8b0733b0265d2d04d4c3d565aa5c0581af1db20
baker <783009838@qq.com>
2017-12-05 10:17:34 +0800

    configServer modify properties cn test4

--- microservice-config-client/src/main/java/com/ithz/cloud/controller/ConfigController.java
+++ microservice-config-client/src/main/java/com/ithz/cloud/controller/ConfigController.java
@@ -1,12 +1,15 @@
 package com.ithz.cloud.controller;
 
+import jdk.nashorn.internal.ir.annotations.Reference;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author baker
  */
+@RefreshScope
 @RestController
 public class ConfigController {
 
